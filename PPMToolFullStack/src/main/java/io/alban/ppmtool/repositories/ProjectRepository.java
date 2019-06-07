27a973f064d115861f6672cc5f1be5f9b284b4cb
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-06-07 11:59:51 +0200

    Find Project by Identifier

--- PPMToolFullStack/src/main/java/io/alban/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/io/alban/ppmtool/repositories/ProjectRepository.java
@@ -8,7 +8,6 @@ import io.alban.ppmtool.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 	
-	@Override
-	Iterable<Project> findAllById(Iterable<Long> iterable);
+	Project findByProjectIdentifier(String projectIdentifier);
 	
 }
