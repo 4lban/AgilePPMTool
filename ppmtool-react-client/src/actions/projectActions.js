cb7be6e582af03257252894091822f5efcf407e8
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-06-14 18:55:58 +0200

    BUG FIX Strange Update Behaviour

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
-import { async } from "q";
 
 export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
@@ -18,7 +21,7 @@ export const getProjects = () => async dispatch => {
   const res = await axios.get("http://localhost:8080/api/project/all");
   dispatch({
     type: GET_PROJECTS,
-    payload: res.datad
+    payload: res.data
   });
 };
 
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -20,6 +20,7 @@ class Dashboard extends Component {
               <h1 className="display-4 text-center">Projects</h1>
               <br />
               <CreateProjectButton />
+
               <br />
               <hr />
               {projects.map(project => (
