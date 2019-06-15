b5adb7a5e9160e893ab56a6af736bef2f854ee30
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-06-16 01:05:08 +0200

    BUG FIX ProjectTask priority, projectIdentifier, PTSequence

--- PPMToolFullStack/src/main/java/io/alban/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/alban/ppmtool/services/ProjectTaskService.java
@@ -28,15 +28,17 @@ public class ProjectTaskService {
 		Integer BacklogSequence = backlog.getPTSequence();
 		// Update the BL SEQUENCE
 		BacklogSequence++;
+		backlog.setPTSequence(BacklogSequence);
 
 		// Add Sequence to PT
 		projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
 		projectTask.setProjectIdentifier(projectIdentifier);
 
 		// INITIAL priority when priority null
-//		if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
-//			projectTask.setPriority(3);
-//		}
+		if (projectTask.getPriority() == null) {
+			// In the future we need projectTask.getPriority()==0 to handle the form
+			projectTask.setPriority(3);
+		}
 
 		// INITIAL status when status is null
 		if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
