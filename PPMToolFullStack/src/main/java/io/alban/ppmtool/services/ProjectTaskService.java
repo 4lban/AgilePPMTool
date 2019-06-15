75240728a7ea770fce7d95a98bc0ddd8ebbe740e
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-06-16 00:53:08 +0200

    Persist Project Task (Bug fix pending setPriority)

--- PPMToolFullStack/src/main/java/io/alban/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/alban/ppmtool/services/ProjectTaskService.java
@@ -3,6 +3,7 @@ package io.alban.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.alban.ppmtool.domain.Backlog;
 import io.alban.ppmtool.domain.ProjectTask;
 import io.alban.ppmtool.repositories.BacklogRepository;
 import io.alban.ppmtool.repositories.ProjectTaskRepository;
@@ -16,14 +17,31 @@ public class ProjectTaskService {
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
 
-	public ProjectTask addProjectTask() {
+	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+		// Exceptions: Project not found
+
 		// PTs to be added to a specific project, project != null
+		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
 		// set the bl to pt
+		projectTask.setBacklog(backlog);
 		// we want our project sequence to be like this: IDPRO-1 IDPRO-2
+		Integer BacklogSequence = backlog.getPTSequence();
 		// Update the BL SEQUENCE
+		BacklogSequence++;
+
+		// Add Sequence to PT
+		projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
 
 		// INITIAL priority when priority null
-		// INITIAL status when status is null
+//		if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+//			projectTask.setPriority(3);
+//		}
 
+		// INITIAL status when status is null
+		if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+			projectTask.setStatus("TO_DO");
+		}
+		return projectTaskRepository.save(projectTask);
 	}
 }
