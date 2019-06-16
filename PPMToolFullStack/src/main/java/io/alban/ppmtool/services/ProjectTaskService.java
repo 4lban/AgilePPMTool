aa99c9a74655f0675275a929573025a284735495
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-06-16 22:13:15 +0200

    BUG FIX delete operation, improved backlogproject task rel

--- PPMToolFullStack/src/main/java/io/alban/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/alban/ppmtool/services/ProjectTaskService.java
@@ -1,7 +1,5 @@
 package io.alban.ppmtool.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -99,13 +97,8 @@ public class ProjectTaskService {
 	}
 
 	public void deletePTByProjectSequence(String backlog_id, String pt_id) {
+
 		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
-
-		Backlog backlog = projectTask.getBacklog();
-		List<ProjectTask> pts = backlog.getProjectTasks();
-		pts.remove(projectTask);
-		backlogRepository.save(backlog);
-
 		projectTaskRepository.delete(projectTask);
 	}
 }
